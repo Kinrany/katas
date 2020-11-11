f2f0d402bf8e1813a63d3263b27c89670eb26926
Ruslan Fadeev <kinrany@yandex.com>
2020-11-11 22:54:28 +0300

    Refactor Rust solution for kata02
    Replace custom 'parse' with standard trait FromStr

--- kata02_codekata_data_munging/rust/src/main.rs
+++ kata02_codekata_data_munging/rust/src/main.rs
@@ -14,20 +14,33 @@ struct Day {
 }
 
 impl Day {
-  fn parse(line: &str) -> Option<Self> {
+  fn temperature_spread(&self) -> u32 {
+    self.max_temperature - self.min_temperature
+  }
+}
+
+impl std::str::FromStr for Day {
+  type Err = ();
+
+  fn from_str(line: &str) -> Result<Self, Self::Err> {
     let parts = line.split_whitespace().collect::<Vec<_>>();
-    let day_number = parts.get(0).and_then(|&s| s.parse::<u32>().ok())?;
-    let max_temperature = parts.get(1).and_then(|&s| s.parse::<u32>().ok())?;
-    let min_temperature = parts.get(2).and_then(|&s| s.parse::<u32>().ok())?;
-    Some(Self {
+    let day_number = parts
+      .get(0)
+      .and_then(|&s| s.parse::<u32>().ok())
+      .ok_or(())?;
+    let max_temperature = parts
+      .get(1)
+      .and_then(|&s| s.parse::<u32>().ok())
+      .ok_or(())?;
+    let min_temperature = parts
+      .get(2)
+      .and_then(|&s| s.parse::<u32>().ok())
+      .ok_or(())?;
+    Ok(Self {
       day_number,
       max_temperature,
       min_temperature,
     })
-  }
-
-  fn temperature_spread(&self) -> u32 {
-    self.max_temperature - self.min_temperature
   }
 }
 
@@ -36,7 +49,7 @@ fn main(args: Args) -> Result<(), Error> {
   let text = std::fs::read_to_string(args.file)?;
   let day_with_smallest_spread = text
     .lines()
-    .filter_map(Day::parse)
+    .filter_map(|line| line.parse().ok())
     .min_by_key(Day::temperature_spread);
   match day_with_smallest_spread {
     Some(day) => println!(

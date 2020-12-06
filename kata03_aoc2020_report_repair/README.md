5a981025ba8dde1d5d1cb1624914e89ce4cb7655
Ruslan Fadeev <kinrany@yandex.com>
2020-12-06 16:10:15 +0300

    Add part 2 of kata 03 with a Rust solution

--- kata03_aoc2020_report_repair/README.md
+++ kata03_aoc2020_report_repair/README.md
@@ -7,3 +7,7 @@ Source: [adventofcode.com/2020/day/1](https://adventofcode.com/2020/day/1)
 > Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
 
 [expense report]: https://adventofcode.com/2020/day/1/input
+
+## Part 2
+
+> The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find **three** numbers in your expense report that meet the same criteria.
--- kata03_aoc2020_report_repair/rust/src/main.rs
+++ kata03_aoc2020_report_repair/rust/src/main.rs
@@ -1,13 +1,64 @@
 use anyhow::{bail, Error};
-use std::collections::HashSet;
+use std::{collections::HashSet, str::FromStr};
+
+#[derive(Debug)]
+enum Part {
+  One,
+  Two,
+}
+
+impl FromStr for Part {
+  type Err = Error;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "one" => Ok(Part::One),
+      "two" => Ok(Part::Two),
+      _ => bail!("Unknown part: {}", s),
+    }
+  }
+}
 
 #[derive(Debug, structopt::StructOpt)]
 struct Args {
   #[structopt(short = "f", long = "file")]
   file: std::path::PathBuf,
+  #[structopt(short = "p", long = "part", default_value = "one")]
+  part: Part,
 }
 
 const YEAR: u32 = 2020;
+
+fn part1(numbers: Vec<u32>) -> Result<u32, Error> {
+  let numbers = numbers.into_iter().collect::<HashSet<_>>();
+  let entry = numbers
+    .iter()
+    .find(|&x| numbers.contains(&(YEAR - x)))
+    .map(|x| *x);
+
+  match entry {
+    None => bail!("Couldn't find two entries that add up to {}", YEAR),
+    Some(x) => Ok(x * (YEAR - x)),
+  }
+}
+
+fn part2(numbers: Vec<u32>) -> Result<u32, Error> {
+  let set = numbers.iter().map(|&x| x).collect::<HashSet<u32>>();
+
+  for &x in numbers.iter() {
+    for &y in numbers.iter() {
+      if x + y > YEAR {
+        continue;
+      }
+      let z = YEAR - x - y;
+      if set.contains(&z) {
+        return Ok(x * y * z);
+      }
+    }
+  }
+
+  bail!("Couldn't find three entries that add up to {}", YEAR);
+}
 
 #[paw::main]
 fn main(args: Args) -> Result<(), Error> {
@@ -15,22 +66,21 @@ fn main(args: Args) -> Result<(), Error> {
   let numbers = text
     .lines()
     .map(&str::parse)
-    .collect::<Result<HashSet<u32>, _>>()?;
-  let entry = numbers
-    .iter()
-    .find(|&x| numbers.contains(&(YEAR - x)))
-    .map(|x| *x);
+    .collect::<Result<Vec<u32>, _>>()?;
 
-  if entry.is_none() {
-    bail!("Couldn't find two entries that add up to {}", YEAR);
-  }
+  let result = match args.part {
+    Part::One => part1(numbers),
+    Part::Two => part2(numbers),
+  };
 
-  match entry {
-    None => bail!("Couldn't find two entries that add up to {}", YEAR),
-    Some(x) => {
-      let answer = x * (YEAR - x);
+  match result {
+    Ok(answer) => {
       println!("Answer: {}", answer);
-      Ok(())
+    }
+    Err(error) => {
+      println!("Error: {}", error);
     }
   }
+
+  Ok(())
 }
